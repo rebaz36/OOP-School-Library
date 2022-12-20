e62faf14f0c5c66472e5b265d8b732a1c29e23ba
rebaz36 <rebaz415@gmail.com>
2022-12-20 19:59:35 +0300

    add correct_name method

--- person.rb
+++ person.rb
@@ -1,9 +1,12 @@
-class Person
+require_relative './nameable/nameable_decorator'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id # getters for @id, @name, and @age
 
   def initialize(age, name: 'Unknown', parent_permission: true)
-    @id = generate_id # generate a unique ID for the person
+    super()
+    @id = Random.rand(1..1000) # generate a unique ID for the person
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -13,14 +16,15 @@ class Person
     is_of_age? || @parent_permission
   end
 
+  def correct_name
+    @name
+  end
+
   private
 
   def is_of_age? # rubocop:disable Naming/PredicateName
     @age >= 18
   end
 
-  def generate_id
-    # generate a unique ID for the person
-    Random.rand(1..1000)
-  end
+
 end
