a4ae6b631eb61c7c9875ae6f0710d495a275031a
rebaz36 <rebaz415@gmail.com>
2023-01-04 01:48:55 +0300

    fix errors in student class

--- src/student.rb
+++ src/student.rb
@@ -1,11 +1,15 @@
+#!/bin/env ruby
+# encoding: utf-8 # rubocop:disable Style/encoding
+
 require_relative './add'
 
 class Student < Person
-  attr_reader :classroom, :name, :id
+  attr_reader :classroom, :name, :id, :age, :parent_permission
 
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+  def initialize(age, classroom, name, parent_permission: false)
     super(age, name, parent_permission)
     @classroom = classroom
+    @parent_permission = parent_permission
   end
 
   def play_hooky
