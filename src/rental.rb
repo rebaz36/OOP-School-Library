b85eb96d3a5b17cfe16f143a1233f0ea37412ba1
rebaz36 <rebaz415@gmail.com>
2023-01-04 01:48:39 +0300

    update rental class

--- src/rental.rb
+++ src/rental.rb
@@ -1,17 +1,18 @@
 require 'date'
 
 class Rental
-  attr_accessor :date, :book, :customer
+  attr_accessor :date, :title, :name, :id
 
-  def initialize(book, customer, date: Date.today)
+  def initialize(book_title, customer_name, customer_id, date = Date.today)
     @date = date
-    @customer = customer.name
-    customer.rentals.push(self)
-    book.rentals.push(self)
-    @book = book.title
+    @name = customer_name
+    @id = customer_id
+    @title = book_title
+    # customer.rentals.push(self)
+    # book.rentals.push(self)
   end
 
   def rentals
-    "#{@book}: #{@customer} #{@date}"
+    "#{@title}: #{@name} #{@date}"
   end
 end
