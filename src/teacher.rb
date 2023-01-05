11e0feef3815fe5735ca6b20ddb452c00ddbea5c
rebaz36 <rebaz415@gmail.com>
2023-01-05 16:27:54 +0300

    update teacher class

--- src/teacher.rb
+++ src/teacher.rb
@@ -3,7 +3,7 @@ require_relative './add'
 class Teacher < Person
   attr_accessor :specialization, :id
 
-  def initialization(age, name, specialization: true)
+  def initialization(age, name, specialization)
     super(age, name, parent_permission: true)
     @specialization = specialization
   end
